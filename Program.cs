a744b01e2ffc7d3aa53c4c25fb1d2be727bfec0d
Nastya <anastasiya.komova@mail.ru>
2022-11-30 23:31:53 +0300

    задача 4

--- Program.cs
+++ Program.cs
@@ -13,3 +13,19 @@ else
     Console.WriteLine("Число большее " + num2 + ", а меньшее " + num1);
 }
 */
+
+//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+/* Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int max = num1;
+
+if (num2 > num1) max = num2;
+if (num3 > num2) max = num3;
+
+Console.WriteLine("Максимальное число: " + max); */
