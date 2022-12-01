de1e59d19c281a7e8edc74da405947a0643f2863
Nastya <anastasiya.komova@mail.ru>
2022-12-01 21:19:51 +0300

    задача 8

--- Program.cs
+++ Program.cs
@@ -45,3 +45,17 @@ else
 Console.WriteLine("Введенное число нечетное");
 }
 */
+
+// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+/* Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int current = 2;
+
+while (current <= num)
+{
+  Console.Write(current + " ");
+  current+=2;
+}
+*/
